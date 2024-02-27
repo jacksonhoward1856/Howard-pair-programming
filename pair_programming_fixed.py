2e0947df14d7f7221e60482c99a3c11b2c35bc00
Max Ercolani <ercolani@mines.edu>
2024-02-27 09:10:28 -0700

    Added feedback and test function

--- pair_programming_fixed.py
+++ pair_programming_fixed.py
@@ -2,3 +2,17 @@ def feet_and_inches_to_meters(feet, inches):
     total_inches = feet * 12 + inches
     meters = total_inches * 0.0254
     return meters
+
+# Good function, could use more comments/documentation
+
+# Test 1
+def test_1():
+    myheight = feet_and_inches_to_meters(5,10)
+    print(myheight)
+    return myheight
+
+# Test 2
+def test_2():
+    zero = feet_and_inches_to_meters(0,0)
+    print(zero)
+    return zero
